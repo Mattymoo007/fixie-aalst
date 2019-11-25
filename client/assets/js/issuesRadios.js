2f553712a8970426f1a5d7430110027cdd2445fa
Janaverl <36476740+Janaverl@users.noreply.github.com>
2019-11-25 22:58:20 +0100

    radioButtons to add issues

---
+++ client/assets/js/issuesRadios.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+    // todo: fetch this data from the database
+    const issues = [
+        {"description": "flat tyre",
+        "class": 'tyre',
+        "time": 60},
+        {"description": "breaks",
+        "class": "breaks",
+        "time": 60},
+        {"description": "fix my whole bike",
+        "class": "whole",
+        "time": 60}
+    ]
+    
+    // show the issues as radiobuttons
+    let radios = '<div>';
+    issues.forEach(issue => {
+        radios += `<label for="${issue['class']}"><input type="radio" name="issue" value="${issue['class']}">${issue['description']}</label>`;
+    });
+    radios += '</div>';
+    $('#typeBike').after(radios);
+    console.log('dit is een test ${issues}');
+});
--- client/assets/js/repairform.js
+++ client/assets/js/repairform.js
@@ -20,38 +20,37 @@ $( document ).ready(function() {
 
     // toggle between the steps
     function toggleBetweenSteps(out, enter){
-        $('.step'+enter).delay(100).fadeIn(100);
-        $('.process li'+enter).addClass('active');
+        $('.step'+enter).delay(200).fadeIn(100);
         $('.step'+out).fadeOut(100); 
+        $('.process li'+enter).addClass('active');
         $('.process li'+out).removeClass('active');
     }
-
     $('#login').click(function(e){
         console.log("here we will need a function to check if login-values are correct");
-        toggleBetweenSteps('.one', '.two'); 
+        toggleBetweenSteps('.login', '.issue'); 
     });
     $('#register').click(function(e){
         console.log("here we will need a function to check if the values and POST to the back and");
-        toggleBetweenSteps('.one', '.two');
-    });
-    $('.two .back').click(function(e){
-        toggleBetweenSteps('.two', '.one');
-    });
-    $('.two .next').click(function(e){
-        toggleBetweenSteps('.two', '.three');
-    });
-    $('.three .back').click(function(e){
-        toggleBetweenSteps('.three', '.two');
-    });
-    $('.three .next').click(function(e){
-        toggleBetweenSteps('.three', '.four');
-    });
-    $('.four .back').click(function(e){
-        toggleBetweenSteps('.four', '.three');
+        toggleBetweenSteps('.login', '.issue');
     });
 
-    // end of toggle between the steps
-
+    $('.issue .back').click(function(e){
+        toggleBetweenSteps('.issue', '.login');
+    });
+    $('.issue .next').click(function(e){
+        console.log("here we will need a function to check if the bike information is filled in and if at least one value is selected");
+        toggleBetweenSteps('.issue', '.date');
+    });
+    $('.date .back').click(function(e){
+        toggleBetweenSteps('.date', '.issue');
+    });
+    $('.date .next').click(function(e){
+        console.log('the calender is not there yet');
+        toggleBetweenSteps('.date', '.summary');
+    });
+    $('.summary .back').click(function(e){
+        toggleBetweenSteps('.summary', '.issue');
+    });
     
 
 
