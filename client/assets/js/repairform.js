4405007a0dba3a4e2d90ede7fabffc9af83ec999
Janaverl <36476740+Janaverl@users.noreply.github.com>
2019-11-25 21:03:59 +0100

    form: toggle between the steps

--- client/assets/js/repairform.js
+++ client/assets/js/repairform.js
@@ -1,17 +1,58 @@
 $( document ).ready(function() {
     console.log("the dom is ready");
 
-    // on click of the register-button, show the register-fields
-    $("#register").on("click", function(){
-        $(".register").toggle( "slow" );
-        $("#login").toggle( "slow" );
-        
+    // toggle between login and registration on click and display the active link to the form as active
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#register-form").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+        $("#register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    // end of toggle between login and registration on click and display the active link to the form as active
+
+    // toggle between the steps
+    function toggleBetweenSteps(out, enter){
+        $('.step'+enter).delay(100).fadeIn(100);
+        $('.process li'+enter).addClass('active');
+        $('.step'+out).fadeOut(100); 
+        $('.process li'+out).removeClass('active');
+    }
+
+    $('#login').click(function(e){
+        console.log("here we will need a function to check if login-values are correct");
+        toggleBetweenSteps('.one', '.two'); 
+    });
+    $('#register').click(function(e){
+        console.log("here we will need a function to check if the values and POST to the back and");
+        toggleBetweenSteps('.one', '.two');
+    });
+    $('.two .back').click(function(e){
+        toggleBetweenSteps('.two', '.one');
+    });
+    $('.two .next').click(function(e){
+        toggleBetweenSteps('.two', '.three');
+    });
+    $('.three .back').click(function(e){
+        toggleBetweenSteps('.three', '.two');
+    });
+    $('.three .next').click(function(e){
+        toggleBetweenSteps('.three', '.four');
+    });
+    $('.four .back').click(function(e){
+        toggleBetweenSteps('.four', '.three');
     });
 
-    // $( "button" ).click(function() {
-    //     $( "p" ).toggle( "slow" );
-    //   });
+    // end of toggle between the steps
 
+    
 
 
   });
