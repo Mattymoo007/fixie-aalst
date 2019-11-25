efcad0eeb3fd66bd4f9a881b9cdcaf1b2daca84d
Janaverl <jana.verlinde@hotmail.com>
2019-11-25 16:16:27 +0100

    rapairform

--- client/js/repairform.js
+++ client/js/repairform.js
@@ -1,4 +1,17 @@
 $( document ).ready(function() {
+    console.log("the dom is ready");
 
-    
+    // on click of the register-button, show the register-fields
+    $("#register").on("click", function(){
+        $(".register").toggle( "slow" );
+        $("#login").toggle( "slow" );
+        
+    });
+
+    // $( "button" ).click(function() {
+    //     $( "p" ).toggle( "slow" );
+    //   });
+
+
+
   });
