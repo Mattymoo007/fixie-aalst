efbdc81ad9a984140fd7aee0a6b0d96d2c839021
Janaverl <36476740+Janaverl@users.noreply.github.com>
2019-11-26 15:48:38 +0100

    add a dragable list with new events

--- client/assets/js/calender.js
+++ client/assets/js/calender.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-
     
 // TODO: this information needs to come from the database
 
@@ -46,32 +45,63 @@ const allTheEvents = [
         title: 'timeslot taken',
         start: '2019-11-25T10:00:00',
         end: '2019-11-25T11:00:00'
-    },
-    {
-        start: '2019-11-24T18:00:00',
-        end: '2014-11-25T08:00:00',
-        rendering: 'background'
     }
     ];
 
 
+    var Calendar = FullCalendar.Calendar;
+    var Draggable = FullCalendarInteraction.Draggable
+
+    // the dragable list
+    var containerEl = document.getElementById('external-events-list');
+    new Draggable(containerEl, {
+      itemSelector: '.fc-event',
+      eventData: function(eventEl) {
+        return {
+          title: eventEl.innerText.trim()
+        }
+      }
+    });
+
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'dayGrid', 'timeGrid' ],
-      header: {
+        plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+        header: {
         right: 'prev,next today', //positions the the prev/next button on the right 
         center: 'title', //sets the title of the month to center
         left: 'dayGridMonth,timeGridWeek,timeGridDay'
         // left: 'month,basicWeek,basicDay' //positions the the prev/next button on the left 
         // left: 'month' //positions the the prev/next button on the left 
       },
+    //   defaultView: 'timeGridWeek',
       // defaultDate: '2018-02-16',
       navLinks: true, // click on day/week names to navigate views
       editable: true,
       eventLimit: true, // add "more" link when there are too many events in a day
       events: allTheEvents,
-      eventOverlap: false // to avoid overlapping
+      eventOverlap: false, // to avoid overlapping
+      businessHours: [
+        {
+          daysOfWeek: [ 1, 2, 3, 4 ], // Monday, Tuesday, Wednesday
+          startTime: '08:00', // 8am
+          endTime: '18:00' // 6pm
+        },
+        {
+          daysOfWeek: [ 4, 5 ], // Thursday, Friday
+          startTime: '08:00', // 10am
+          endTime: '16:00' // 4pm
+        }
+      ],
+      nowIndicator: true,
+      
+      // the drop-function:  
+      editable: true,
+      droppable: true, // this allows things to be dropped onto the calendar
+      drop: function(arg) {
+        // remove the element from the "Draggable Events" list
+        arg.draggedEl.parentNode.removeChild(arg.draggedEl);
+      }
     });
 
     calendar.render();
