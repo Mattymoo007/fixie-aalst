aed108e8078a4dd39308f93d4f23b90423943c0d
Mattymoo007 <mattymoo007@gmail.com>
2019-11-26 16:06:49 +0100

    Fadein on page load

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,10 +1,15 @@
-// Code to include components in HTML files
-$(document).ready(function() {
-  $(function() {
+$(document).ready(function () {
+
+  // Code to include components in HTML files -- Experiment
+  $(function () {
     var includes = $("[data-include]");
-    jQuery.each(includes, function() {
+    jQuery.each(includes, function () {
       var file = "../assets/components/" + $(this).data("include") + ".html";
       $(this).load(file);
     });
   });
+
+  // Fades in whole document on load
+  $("body").css("display", "none");
+  $("body").fadeIn(700);
 });
