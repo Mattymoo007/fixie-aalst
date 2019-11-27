dc60edd8e9111ae376d87739368e1afabeccbd76
Mattymoo007 <mattymoo007@gmail.com>
2019-11-27 10:48:05 +0100

    Registration form front page

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
 
-  // Code to include components in HTML files -- Experiment
-  $(function () {
-    var includes = $("[data-include]");
-    jQuery.each(includes, function () {
-      var file = "../assets/components/" + $(this).data("include") + ".html";
-      $(this).load(file);
-    });
-  });
-
   // Fades in whole document on load
   $("body").css("display", "none");
   $("body").fadeIn(700);
