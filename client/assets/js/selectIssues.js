9fb63063ae514f31fd0540275b6ccf6901546326
Mattymoo007 <mattymoo007@gmail.com>
2019-11-28 14:32:54 +0100

    Added select menu

--- client/assets/js/selectIssues.js
+++ client/assets/js/selectIssues.js
@@ -1,25 +1,16 @@
-$( document ).ready(function() {
-    // todo: fetch this data from the database
-    const issues = [
-        {"description": "flat tyre",
-        "class": 'tyre',
-        "time": 30,
-        "cost": 50},
-        {"description": "breaks",
-        "class": "breaks",
-        "time": 60,
-        "cost": 100},
-        {"description": "fix my whole bike",
-        "class": "whole",
-        "time": 180,
-        "cost": 200}
-    ]
-    
-    // show the issues as radiobuttons
-    let radios = '';
-    issues.forEach(issue => {
-        radios += `<label for="${issue['class']}"><input type="checkbox" name="issue" value="${issue['class']}">${issue['description']}</label>`;
-    });
-    $('#selectIssue').append(radios);
-    console.log('dit is een test ${issues}');
+$(document).ready(function() {
+  // todo: fetch this data from the database
+  const issues = [
+    { description: "flat tyre", class: "tyre", time: 30, cost: 50 },
+    { description: "breaks", class: "breaks", time: 60, cost: 100 },
+    { description: "fix my whole bike", class: "whole", time: 180, cost: 200 }
+  ];
+
+  // show the issues as radiobuttons
+  let selects = "";
+  issues.forEach(issue => {
+    // selects += `<label for="${issue['class']}"><input type="checkbox" name="issue" value="${issue['class']}">${issue['description']}</label>`;
+    selects += `<option>${issue["description"]}</option>hello`;
+  });
+  $("#selectIssue").append(selects);
 });
