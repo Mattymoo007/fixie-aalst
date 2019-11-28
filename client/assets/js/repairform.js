2fa904d450041ccca047a3a7b4f1bbef8f1ea14a
Mattymoo007 <mattymoo007@gmail.com>
2019-11-28 11:21:34 +0100

    Toggle buttons

--- client/assets/js/repairform.js
+++ client/assets/js/repairform.js
@@ -13,82 +13,39 @@ $(document).ready(function() {
   $(".form .buttons .previous").on("click", () => {
     movePage("previous");
   });
+  $(".form .buttons .previous").hide();
 
   function movePage(direction) {
     event.preventDefault();
 
+    var currentPage = $(".page.show").index();
     var pages = $(".page");
-
-    pages.each(function(i) {
-      if ($(this).hasClass("show")) {
-        $(this).removeClass("show");
-        return (currentPage = i);
-      }
+    pages.each(function() {
+      $(this).removeClass("show");
     });
+
     if (direction === "next") {
-      if (currentPage > 2) {
-        $(".form .buttons .next").hide();
+      pages.eq(currentPage + 1).addClass("show");
+      buttonToggle();
+    } else if (direction === "previous") {
+      pages.eq(currentPage - 1).addClass("show");
+      buttonToggle();
+    }
+
+    function buttonToggle() {
+      var newPage = $(".page.show").index();
+      if (newPage >= 1 && newPage < 3) {
+        $(".form .buttons .next").show();
         $(".form .buttons .previous").show();
       } else {
-        pages.eq(currentPage + 1).addClass("show");
+        if (newPage <= 0) {
+          $(".form .buttons .next").show();
+          $(".form .buttons .previous").hide();
+        } else if (newPage >= 3) {
+          $(".form .buttons .next").hide();
+          $(".form .buttons .previous").show();
+        }
       }
-    } else if (direction === "previous") {
-      if (currentPage === 1) {
-        $(".form .buttons .previous").hide();
-        $(".form .buttons .next").show();
-      }
-      pages.eq(currentPage - 1).addClass("show");
     }
   }
-
-  // // toggle between login and registration on click and display the active link to the form as active
-  // $('#login-form-link').click(function (e) {
-  //     $("#login-form").delay(100).fadeIn(100);
-  //     $("#register-form").fadeOut(100);
-  //     $('#register-form-link').removeClass('active');
-  //     $(this).addClass('active');
-  //     e.preventDefault();
-  // });
-  // $('#register-form-link').click(function (e) {
-  //     $("#register-form").delay(100).fadeIn(100);
-  //     $("#login-form").fadeOut(100);
-  //     $('#login-form-link').removeClass('active');
-  //     $(this).addClass('active');
-  //     e.preventDefault();
-  // });
-  // // end of toggle between login and registration on click and display the active link to the form as active
-
-  // // toggle between the steps
-  // function toggleBetweenSteps(out, enter) {
-  //     $('.step' + enter).delay(200).fadeIn(100);
-  //     $('.step' + out).fadeOut(100);
-  //     $('.process li' + enter).addClass('active');
-  //     $('.process li' + out).removeClass('active');
-  // }
-  // $('#login').click(function (e) {
-  //     console.log("here we will need a function to check if login-values are correct");
-  //     toggleBetweenSteps('.login', '.issue');
-  // });
-  // $('#register').click(function (e) {
-  //     console.log("here we will need a function to check if the values and POST to the back and");
-  //     toggleBetweenSteps('.login', '.issue');
-  // });
-
-  // $('.issue .back').click(function (e) {
-  //     toggleBetweenSteps('.issue', '.login');
-  // });
-  // $('.issue .next').click(function (e) {
-  //     console.log("here we will need a function to check if the bike information is filled in and if at least one value is selected");
-  //     toggleBetweenSteps('.issue', '.date');
-  // });
-  // $('.date .back').click(function (e) {
-  //     toggleBetweenSteps('.date', '.issue');
-  // });
-  // $('.date .next').click(function (e) {
-  //     console.log('the calender is not there yet');
-  //     toggleBetweenSteps('.date', '.summary');
-  // });
-  // $('.summary .back').click(function (e) {
-  //     toggleBetweenSteps('.summary', '.issue');
-  // });
 });
